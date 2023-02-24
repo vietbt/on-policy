54e61df08adb20a3e20ce9f7e949e77bb5c49fc6
vietbt <vietbt6@gmail.com>
2023-02-25 06:19:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,9 @@ import os
 from setuptools import setup, find_packages
 import setuptools
 
-def get_version() -> str:
-    # https://packaging.python.org/guides/single-sourcing-package-version/
-    init = open(os.path.join("onpolicy", "__init__.py"), "r").read().split()
-    return init[init.index("__version__") + 2][1:-1]
-
 setup(
     name="onpolicy",  # Replace with your own username
-    version=get_version(),
+    version="1.1.0",
     description="on-policy algorithms of marlbenchmark",
     long_description=open("README.md", encoding="utf8").read(),
     long_description_content_type="text/markdown",
