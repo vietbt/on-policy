26f26a2174131e88888a8859604d170bc660d647
vietbt <vietbt6@gmail.com>
2023-02-25 06:20:35 +0800

    Update __init__.py

--- onpolicy/__init__.py
+++ onpolicy/__init__.py
@@ -1,13 +1,12 @@
 from onpolicy import *
 
-
 __version__ = "0.1.0"
 
-# __all__ = [
-#     "algorithms",
-#     "envs",
-#     "runner",
-#     "scripts",
-#     "utils",
-#     "config",
-# ]
+__all__ = [
+    "algorithms",
+    "envs",
+    "runner",
+    "scripts",
+    "utils",
+    "config",
+]
